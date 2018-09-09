15ea4042bcb295bb7ad5d204be2fdab88789900d
Anton Bukov <k06aaa@gmail.com>
2018-09-09 06:40:57 +0300

    Add commented infura mainnet providers

--- snark_wrapper/deploy.js
+++ snark_wrapper/deploy.js
@@ -30,6 +30,7 @@ if (typeof web3 !== 'undefined') {
 } else {
   // set the provider you want from Web3.providers
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  // web3 = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws'));
 }
 
 
